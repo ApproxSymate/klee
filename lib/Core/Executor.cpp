cefa2ff041c645046886379051763ba9bc074c9c
Himeshi <himeshidslv@gmail.com>
2018-08-02 12:13:14 +0800

    Add round to math functions supported

--- lib/Core/Executor.cpp
+++ lib/Core/Executor.cpp
@@ -3947,7 +3947,8 @@ static std::set<std::string> okExternals(okExternalsList,
                                          okExternalsList + 
                                          (sizeof(okExternalsList)/sizeof(okExternalsList[0])));
 
-static const char *mathCallNames[] = { "sin", "cos", "sqrt", "abs", "fabs" };
+static const char *mathCallNames[] = { "sin", "cos",  "sqrt",
+                                       "abs", "fabs", "round" };
 
 static std::set<std::string> externalMathCallsList(
     mathCallNames,
